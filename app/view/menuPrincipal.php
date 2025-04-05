321ab6a044feb627b45c3a50c65db1642008c622
PabloIC <iravedracambre1992@gmail.com>
2025-04-05 19:35:35 +0200

    Menú Principal añadido

---
+++ app/view/menuPrincipal.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROYECTO FIN DE CICLO - Menú Principal</title>
+</head>
+<body>
+    <h1>Comienza el proyecto</h1>
+</body>
+</html>
