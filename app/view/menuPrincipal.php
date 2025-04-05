8d773f0d8ea35531ace94d44c89f063f5bbccdaa
PabloIC <iravedracambre1992@gmail.com>
2025-04-06 00:39:46 +0200

    Añadido CSS menú principal

--- app/view/menuPrincipal.php
+++ app/view/menuPrincipal.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROYECTO FIN DE CICLO - Menú Principal</title>
+    <link rel="stylesheet" href="css/estilosPrincipal.css">
 </head>
 <body>
     <h1>Comienza el proyecto</h1>
+    <p>Emporio nostálgico</p>
 </body>
 </html>
