82be8d43786129383c79340c2ce1fabefdf213f0
PabloIC <iravedracambre1992@gmail.com>
2025-04-06 21:30:10 +0200

    Añadido Bootstrap

--- app/view/menuPrincipal.php
+++ app/view/menuPrincipal.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROYECTO FIN DE CICLO - Menú Principal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" 
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" 
+    crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="css/estilosPrincipal.css">
 </head>
 <body>
-    <h1>Comienza el proyecto</h1>
-    <p>Emporio nostálgico</p>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="imagenesTiendaNostalgica/logoEmporio2.png" alt="spotifun" width="230" height="105">
+              </a>
+          <!-- <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+            <span class="navbar-toggler-icon"></span>
+          </button> -->
+          <div class="collapse navbar-collapse justify-content-start" id="navbarNav">
+            <ul class="navbar-nav flex-col"> 
+              <li class="nav-item mb-3">
+                <a class="nav-link" href="#">Buscar Productos</a>
+              </li>
+              <li class="nav-item me-3">
+                <a class="nav-link" href="#">Sugerencia del día</a>
+              </li>
+              <li class="nav-item me-3">
+                <a class="nav-link" href="#">Quiénes Somos</a>
+              </li>
+              <li class="nav-item me-3">
+                <a class="nav-link" href="#">Contactar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
 </body>
 </html>
